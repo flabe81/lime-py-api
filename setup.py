6f652f7785eeb1415490c824e75b5bad70100ed4
gurkanboll <gustavleander91@hotmail.com>
2015-11-04 18:35:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup, find_packages
 
+setup(name='Limesurvey API',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='',
+      author_email='',
+      url='https://github.com/gurkanboll/lime-py-api',
+      packages=find_packages(),
+     )

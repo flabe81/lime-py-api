7efd1f06e2dbeab042319dad3e2228faf9f11a3f
flabe81 <fabiorex81@gmail.com>
2015-12-09 15:22:21 +0100

    - Example by token - English Translation of the exasmple

--- example_export_response_by_token.py
+++ example_export_response_by_token.py
@@ -5,27 +5,21 @@ import base64  # para encodear la subida de surveys
 import config
 
 # Authentication
-usuario = config.LIME_USER
-clave = config.LIME_KEY
+user = config.LIME_USER
+key = config.LIME_KEY
 url = config.LIME_API_URL
 
 # Build the API
-lime = Api(url, usuario, clave)
+lime = Api(url, user, key)
 
 # SET TOKEN BASE and Survey
 sid = config.LIME_SID
 token = config.LIME_TOKEN_BASE
 
 # GET data - token
-export_res_token = lime.export_responses_by_token(sid, token)
+export_res = lime.export_responses_by_token(sid, token)
 
-# Insert in db
-export_res = export_res_token
-
-## Add Response
-response_to_add = {}
-
-# export_res export_res = lime.export_responses_by_token('999729', 'qdgb2bkiqgqwtvu')
+# Save a in a Json file
 if export_res is not None:
     decoded_string = base64.b64decode(export_res)
     with open("Output.json", "w") as text_file:
